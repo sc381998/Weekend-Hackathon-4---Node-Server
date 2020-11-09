05a55c802f49e18b10b050d03eb77f43f16959f0
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-11-09 23:29:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ const httpServer = http.createServer(handleServer);
 
 function handleServer(req, res) {
   if(req.method == "GET"){
-		if(req.url == "/welcome"){
-      res.statusCode = 200;
-			res.setHeader("Content-Type","text/plain");
-			res.end('Welcome to Dominos!');
-    }
-		if(req.url == "/contact"){
-      res.statusCode = 200;
-			res.setHeader("Content-Type","application/json");
-			res.end('{
-phone: '18602100000',
-email: 'guestcaredominos@jublfood.com'
-}');
-    }
+	if(req.url == "/welcome"){
+     		res.statusCode = 200;
+		res.setHeader("Content-Type","text/plain");
+		res.end('Welcome to Dominos!');
+    	}
+	else if(req.url == "/contact"){
+      		res.statusCode = 200;
+		res.setHeader("Content-Type","application/json");
+		res.end('{phone: '18602100000',email: 'guestcaredominos@jublfood.com'}');
+    	}else{
+		res.statusCode = 404;
+	}
+		
 }
 
 module.exports = httpServer;
