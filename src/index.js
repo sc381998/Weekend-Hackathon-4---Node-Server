2dd9363cf862fa4df4bfb1b988104f769be700e8
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-11-10 16:26:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,9 +13,11 @@ function handleServer(req, res) {
 		else if(req.url == "/contact"){
 	      	res.statusCode = 200;
 			res.setHeader("Content-Type","application/json");
-			res.end("{phone: '18602100000',email: 'guestcaredominos@jublfood.com'}");
+			res.end("{'phone': '18602100000','email': 'guestcaredominos@jublfood.com'}");
 	    }else{
 			res.statusCode = 404;
+		    res.setHeader("Content-Type","text/plain");
+			res.end('Error 404! Not Found');
 		}
   	
 		
